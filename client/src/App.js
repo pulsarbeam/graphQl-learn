c9c5b1b88e6c8557d0f680ec7318cb981928e80f
Rene Groothuis <renegrooth@gmail.com>
2023-06-22 09:10:34 +1200

    added client add and delte

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,31 @@
 import Header from './components/Header'
 import Clients from './components/Clients'
+import Projects from './components/Projects'
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client'
+import AddClientModal from './components/AddClientModal'
+
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        clients: {
+          merge(existing, incoming) {
+            return incoming
+          },
+        },
+        projects: {
+          merge(existing, incoming) {
+            return incoming
+          },
+        },
+      },
+    },
+  },
+})
 
 const client = new ApolloClient({
   uri: 'http://localhost:3000/graphql',
-  cache: new InMemoryCache(),
+  cache,
 })
 
 function App() {
@@ -13,6 +34,8 @@ function App() {
       <ApolloProvider client={client}>
         <Header />
         <div className="container">
+          <AddClientModal />
+          <Projects />
           <Clients />
         </div>
       </ApolloProvider>
